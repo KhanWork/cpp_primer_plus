df8e2c21c1e8f4f10652a1b5bab6b77a5cdbfccb
du.xudong <55368392+KhanWork@users.noreply.github.com>
2022-09-17 17:26:11 +0800

    ch 2
    chapter 2

---
+++ 2_ch_settingOutToCpp/2.1/myfirst.cpp
@@ -0,0 +1,15 @@
+// myfirst.cpp -- displays a message
+
+#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	using namespace std;
+	cout << "Come up and C++ me some time.";
+	cout << endl;
+	cout << "You won't regret it!" << endl;
+
+	printf("printf output\r\n");
+	return 0;
+}
